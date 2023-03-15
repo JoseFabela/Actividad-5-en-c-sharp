f936947acd071a7b8497cd6ff3955bbbc7da2b9d
JoseFabela <jfabela522@gmail.com>
2023-03-14 22:55:18 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 5 en c sharp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Actividad_5_en_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIdentificar = new System.Windows.Forms.Button();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIdentificar
+            // 
+            this.btnIdentificar.Location = new System.Drawing.Point(110, 96);
+            this.btnIdentificar.Name = "btnIdentificar";
+            this.btnIdentificar.Size = new System.Drawing.Size(75, 23);
+            this.btnIdentificar.TabIndex = 0;
+            this.btnIdentificar.Text = "CLICK";
+            this.btnIdentificar.UseVisualStyleBackColor = true;
+            this.btnIdentificar.Click += new System.EventHandler(this.btnIdentificar_Click_1);
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(300, 148);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeros.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INGRESE LA CANTIDAD DEL ARREGLO: EJEMPLO: 1, 10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.btnIdentificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIdentificar;
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Actividad 5 en c sharp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Actividad_5_en_c_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+
+        private void btnIdentificar_Click_1(object sender, EventArgs e)
+        {
+            // Obtener el arreglo de enteros desde el cuadro de texto
+            int[] numeros = txtNumeros.Text.Split(',').Select(int.Parse).ToArray();
+
+            // Mostrar la cantidad de elementos en el arreglo
+            MessageBox.Show($"El arreglo tiene {numeros.Length} elementos.");
+
+            // Mostrar el elemento más grande del arreglo
+            MessageBox.Show($"El elemento más grande del arreglo es {numeros.Max()}.");
+
+            // Mostrar el elemento más pequeño del arreglo
+            MessageBox.Show($"El elemento más pequeño del arreglo es {numeros.Min()}.");
+        }
+    }
+}
+
+
+
